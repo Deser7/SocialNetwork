a9ceb964ca031d3f76fba843a0f1e2087ffe4291
Deser7 <deser7@icloud.com>
2025-11-17 23:25:54 +0400

    создал модель данных

---
+++ SocialNetwork/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  SocialNetwork
+//
+//  Created by Наташа Спиридонова on 17.11.2025.
+//
+
+import Foundation
+
+struct Post {
+    let id: UUID
+    let title: String?
+    let content: String?
+    let imageURL: URL?
+}
