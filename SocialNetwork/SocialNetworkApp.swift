4eb8a4f0405206e0ffaf0c3375e115b99d0d568c
Deser7 <deser7@icloud.com>
2025-11-17 23:18:36 +0400

    создал исходный файл

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Наташа Спиридонова on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
