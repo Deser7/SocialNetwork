6ed1de8267146133a14e38bc51c5b58f55689862
Deser7 <deser7@icloud.com>
2025-11-23 20:53:27 +0400

    заменил сервис по загрузки аватарок

--- SocialNetwork/Services/APIEnum.swift
+++ SocialNetwork/Services/APIEnum.swift
@@ -12,6 +12,6 @@ enum APIEnum {
     static let postsURL = URL(string: "\(baseURL)/posts")
     
     static func avatarURL(for userId: Int) -> URL? {
-        URL(string: "https://picsum.photos/seed/user\(userId)/100/100")
+        URL(string: "https://api.dicebear.com/7.x/avataaars/png?seed=\(userId)&size=100&backgroundColor=b6e3f4")
     }
 }
